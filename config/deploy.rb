7cf4157b258bcaae6263a5f423f91ca85ad2cfe7
Tomas Agrimbau <tomas@theamalgama.com>
2017-03-22 15:57:45 -0300

    Add cron to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,9 +58,7 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      # end
+      exec 'whenever --update-crontab'
     end
   end
 
