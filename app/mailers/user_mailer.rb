51a5b58ac251b4fa4122314acefde5fa97f2f8af
Tomas Agrimbau <tomas@theamalgama.com>
2017-10-27 10:52:44 -0300

    Add Bamba's and Romainas Emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,8 @@ class UserMailer < ApplicationMailer
 
 
 	 @entry = entry
-	 mail_to = 'tomas@amalgama.co,smacagno@gmail.com,rmacagno@hotmail.com,lilitogni29@hotmail.com'
+	 mail_to = 'tomas@amalgama.co,smacagno@gmail.com,rmacagno@hotmail.com,lilitogni29@hotmail.com,
+					rodrigorueda@live.com,Luzz.t@hotmail.com,paulapolo_94@gmail.com'
 	 #mail_to = 'tomas@theamalgama.com'
 	 mail(bcc: mail_to, subject: 'RatAlert - ' + entry.title)
   end
