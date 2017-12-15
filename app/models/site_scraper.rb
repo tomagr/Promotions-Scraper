6917c589c758b97e3a010e7a6759d9570cf9d66d
Tomas Agrimbau <tomas@theamalgama.com>
2017-12-15 03:42:22 -0300

    Add rescue and admin email

--- app/models/site_scraper.rb
+++ app/models/site_scraper.rb
@@ -16,17 +16,29 @@ class SiteScraper < ApplicationRecord
 
 
   def self.save_entry entry
+	 begin
+		analize_entry entry
+	 rescue NoMethodError => error
+		send_admin_error_email error
+	 end
+  end
+
+  def self.analize_entry entry
 	 title = entry.css('h2 a').text
+	 site_id = parse_id_from_uri entry.css('h2 a')[0]['href']
+	 status = entry.css('ul li span').text
 
-	 if title.present?
-		site_id = parse_id_from_uri entry.css('h2 a')[0]['href']
-		status = entry.css('ul li span').text
+	 # And doesn't exists on DB?
+	 if is_available? entry and Entry.find_by_site_id(site_id).nil?
+		entry = Entry.create(:title => title, :status => status, :site_id => site_id)
+		UserMailer.new_entry_email(entry).deliver_now if entry.status.blank?
+	 end
+  end
 
-		# And doesn't exists on DB?
-		if is_available? entry and Entry.find_by_site_id(site_id).nil?
-		  entry = Entry.create(:title => title, :status => status, :site_id => site_id)
-		  UserMailer.new_entry_email(entry).deliver_now if entry.status.blank?
-		end
+  def self.send_admin_error_email error
+	 if Time.now > $last_admin_email_time + 1.day
+		UserMailer.admin_email(error).deliver_now
+		$last_admin_email_time = Time.now
 	 end
   end
 
