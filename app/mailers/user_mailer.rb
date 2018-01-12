9e49133e56f9e38a0553bee88ab1155f7c5c00bc
Tomas Agrimbau <tomas@theamalgama.com>
2018-01-12 16:44:32 -0300

    Add Jose's email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
 
 	 @entry = entry
 	 mail_to = 'tomas@amalgama.co,smacagno@gmail.com,rmacagno@hotmail.com,lilitogni29@hotmail.com,
-					rodrigorueda@live.com,Luzz.t@hotmail.com,ana@amalgama.co'
+					rodrigorueda@live.com,Luzz.t@hotmail.com,ana@amalgama.co,josetedin@gmail.com'
 	 #mail_to = 'tomas@theamalgama.com'
 	 mail(bcc: mail_to, subject: 'RatAlert - ' + entry.title)
   end
