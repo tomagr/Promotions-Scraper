e698f8505b2d21faebe33da43d645a8aacddeacf
Tomas Agrimbau <tomas@theamalgama.com>
2018-01-12 09:51:26 -0300

    Add Ana's email, remove Bambi's :(

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,15 +3,9 @@ class UserMailer < ApplicationMailer
 
   def new_entry_email(entry)
 
-	 #emails = ['tomas@amalgama.co', 'smacagno@gmail.com',
-	 #			  'rmacagno@hotmail.com', 'lilitogni29@hotmail.com',
-	 #			  'rodrigorueda@live.com', 'Luzz.t@hotmail.com']
-	 #emails.each { |email| mail(to: email, subject: 'RatAlert - ' + entry.title) }
-
-
 	 @entry = entry
 	 mail_to = 'tomas@amalgama.co,smacagno@gmail.com,rmacagno@hotmail.com,lilitogni29@hotmail.com,
-					rodrigorueda@live.com,Luzz.t@hotmail.com,paulapolo_94@gmail.com'
+					rodrigorueda@live.com,Luzz.t@hotmail.com,ana@amalgama.co'
 	 #mail_to = 'tomas@theamalgama.com'
 	 mail(bcc: mail_to, subject: 'RatAlert - ' + entry.title)
   end
