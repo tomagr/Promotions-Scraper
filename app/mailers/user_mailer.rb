4c7a666855d61bbdcc7a7fcd65699418838f8b7d
Tomas Agrimbau <tomas@amalgama.co>
2018-09-10 11:51:26 -0300

    Add curvis email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def new_entry_email(entry)
 
 	 @entry = entry
-	 mail_to = 'tomas@amalgama.co,smacagno@gmail.com,rmacagno@gmail.com,lilitogni29@hotmail.com,josetedin@gmail.com'
+	 mail_to = 'tomas@amalgama.co,smacagno@gmail.com,rmacagno@gmail.com,lilitogni29@hotmail.com,josetedin@gmail.com,agus_mllobet@hotmail.com'
 	 #mail_to = 'tomas@theamalgama.com'
 	 mail(bcc: mail_to, subject: 'RatAlert - ' + entry.title)
   end
