15556b6533b08e4fa0c495830d57c3ec98c6a7e9
Tomas Agrimbau <tomas@amalgama.co>
2018-10-17 21:14:06 -0300

    Add config and annotate gem

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -1,15 +1,11 @@
 class ScraperController < ApplicationController
 
-  def index
-    redirect_to "http://beta.alomacain.com"
-  end
-
   def list
     #Get available first
     @entries = Entry.all.order('id DESC').limit(20)
   end
 
   def scrape
-    @results = SiteScraper.scrape_by_url "http://experienciasblack.lanacion.com.ar/"
+    @results = SiteScraper.scrape_by_url Settings.scrapped_site
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'scraper@papensen.com'
 
   def new_entry_email(entry)
-
 	 @entry = entry
-	 mail_to = 'tomas@amalgama.co,smacagno@gmail.com,rmacagno@gmail.com,lilitogni29@hotmail.com,josetedin@gmail.com,agus_mllobet@hotmail.com'
+	 mail_to = Settings.users.receivers
 	 #mail_to = 'tomas@theamalgama.com'
 	 mail(bcc: mail_to, subject: 'RatAlert - ' + entry.title)
   end
