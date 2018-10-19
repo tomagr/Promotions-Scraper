fbefbdd47ae7f4f9bc0d1e1f10b9bcbc1382128f
Tomas Agrimbau <tomas@amalgama.co>
2018-10-19 18:46:47 -0300

    Add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   root 'scraper#index'
 
   get 'scrape', to: 'scraper#scrape'
