2c579b7e7264ec31671f72f16add9ad6b44209a4
Tomas Agrimbau <tomas@amalgama.co>
2019-06-03 14:37:01 -0300

    Add config for web console
    ref #6

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Scraper
   class Application < Rails::Application
+
+		config.load_defaults 5.2
+
+		config.web_console.development_only = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
