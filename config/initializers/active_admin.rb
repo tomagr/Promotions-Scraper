8da6206d1200287f3b41fcdd50d8040da0b10e1a
Tomas Agrimbau <tomas@amalgama.co>
2021-01-18 16:21:03 -0300

    Add favion

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Scraper"
+  config.site_title = "RatAlert Admin"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -169,7 +169,7 @@ ActiveAdmin.setup do |config|
 
   # == Setting a Favicon
   #
-  # config.favicon = 'favicon.ico'
+  config.favicon = 'rat.png'
 
   # == Meta Tags
   #
