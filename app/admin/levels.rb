253de42d4038294f7e01a337ca3191b9690b8688
Tomas Agrimbau <tomas@amalgama.co>
2021-06-19 17:45:16 -0300

    Remove ugliefier

--- app/admin/levels.rb
+++ app/admin/levels.rb
@@ -1,54 +1,26 @@
 ActiveAdmin.register Level do
-  permit_params :title, :status, :featured, :email_sent
+  permit_params :priority, :clue, :code, :solved
 
   ############# Index #############
   index do
     selectable_column
-    column :title do |entity|
-      title = entity.title
-      title += entity.featured? ? ' (*)' : ''
-      title
-    end
-    column :status
-    column :created_at
-    column :updated_at
-    column :released_at
-    column :email_sent
+    column :priority
+    column :clue
+    column :code
+    column :solved
     actions
   end
 
-  filter :title
-  filter :updated_at
-  filter :created_at
 
   ############# Form #############
   form do |f|
     f.inputs do
-      f.input :title
-      f.input :status
-      f.input :featured
-      f.input :email_sent
+      f.input :priority
+      f.input :text
+      f.input :code
+      f.input :solved
     end
     f.actions
-  end
-
-  ############# Show #############
-  show do |entity|
-
-    attributes_table_for entity do
-      row :title
-      row :status
-      row :status
-      row :email_sent
-      row :site_link do
-        link_to entity.site_link, entity.site_link
-      end
-      row :created_at
-      row :updated_at
-      row :released_at
-      row :featured
-    end
-
   end
 
 end
