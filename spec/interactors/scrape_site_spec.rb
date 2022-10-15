5d25b375268f6fd50d1c6ec28120f363ebde6a22
Tomas Agrimbau <tomas@amalgama.co>
2022-10-15 17:48:09 -0300

    Added empty url test

--- spec/interactors/scrape_site_spec.rb
+++ spec/interactors/scrape_site_spec.rb
@@ -8,18 +8,19 @@ RSpec.describe ScrapeSite do
 
 	context 'with correct params' do
 
-		it {
-			expect(interactor).to be_a Nokogiri::XML::NodeSet
-		}
+		it { expect(interactor).to be_a Nokogiri::XML::NodeSet }
+		it { expect(interactor).to all(be_a(Nokogiri::XML::Element)) }
 
-		it {
-			expect(interactor).to all(be_a(Nokogiri::XML::Element))
-		}
-
-		context 'with incorrect params' do
+		context 'without url' do
 			let(:url) { nil }
 			include_examples 'interactor raises an error'
 		end
+
+		context 'with empty url' do
+			let(:url) { '' }
+			include_examples 'interactor raises an error'
+		end
+
 	end
 
 end
