9a08ebab837a9f6e2bce883a967bed43a800f6f9
Tomas Agrimbau <tomas@amalgama.co>
2022-10-15 08:29:08 -0300

    Updated email account password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 		:port => "587",
 		:domain => "gmail.com",
 		:user_name => "ratalert2017@gmail.com",
-		:password => "sinclair2955",
+		:password => "zxsdfkoedztafxbp",
 		:authentication => "plain"
 	}
 
