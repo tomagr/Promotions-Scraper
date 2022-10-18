82465a9c79b0a70c8f884e050f44d692c4d382ae
Tomas Agrimbau <tomas@amalgama.co>
2022-10-18 15:00:03 -0300

    WIP add test for entries

--- spec/interactors/save_entries_spec.rb
+++ spec/interactors/save_entries_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 RSpec.describe SaveEntries do
 
+	let(:url) { Settings.scrapped_site }
 	let(:xml_entries) { ScrapeSite.by(url: url) }
 	let(:interactor) { SaveEntries.save(xml_entries: xml_entries) }
 
 	context 'with correct params' do
 
-		# it { expect(interactor).to be_a Nokogiri::XML::NodeSet }
-		# it { expect(interactor).to all(be_a(Nokogiri::XML::Element)) }
+		context 'xml_entries' do
 
-		context 'without xml_entries' do
-			let(:xml_entries) { nil }
-			include_examples 'interactor raises an error', Error
+			it{
+				# byebug
+			}
 		end
+
+
+	end
+
+
+	context 'with incorrect params' do
+		#
+		# context 'without xml_entries' do
+		# 	let(:xml_entries) { nil }
+		# 	include_examples 'interactor raises an error', Error
+		# end
+		#
+		# context 'without xml_entries' do
+		# 	let(:xml_entries) { nil }
+		# 	include_examples 'interactor raises an error', Error
+		# end
 
 	end
 
