61310b48cc71f4962071b8b75599edebbb69e51b
Tomas Agrimbau <tomas@amalgama.co>
2022-10-21 17:39:33 -0300

    Add puma file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,6 @@
-role :app, %w{deploy@174.129.91.31}
-role :web, %w{deploy@174.129.91.31}
-role :db,  %w{deploy@174.129.91.31}
+set :rails_env, 'production'
+set :branch, 'production'
 
 set :deploy_to, '/srv/rails/scraper'
 
 server '174.129.91.31', user: 'deploy', roles: %w{web app}
-set :rails_env, 'production'
-set :branch, 'production'
